4d17ee34f274ed4b942ea2b8dfeb0626e6cd9336
Vanshika-Dargan <vanshikadargan.vd@gmail.com>
2024-10-10 00:38:06 +0530

    updated the admin routes

---
+++ server/src/modules/admin/controllers/admin-controller.js
@@ -0,0 +1,55 @@
+import AssignmentModel from "../../assignment/models/assignment-model";
+
+export const getAssignmentsTagged = async (req, res, next) => {
+  try {
+    const { adminId } = req.body;
+    const assignments = await AssignmentModel.find({
+        "submissions.submittedTo": adminId,
+      });
+
+    if (assignments.length === 0) {
+      return res.status(404).json({ message: "No assignments found for this admin." });
+    }
+
+   return res.status(200).json(assignments);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateAssignmentStatus = async (req, res, next) => {
+    try {
+      const {action} = req.params;
+      const {assignmentId,adminId} = req.body;
+
+  
+      const assignment = await AssignmentModel.findOne({
+        assignmentId,
+        "submissions.submittedTo": adminId,
+      });
+  
+      if (!assignment) {
+        return res.status(404).json({ message: "Assignment not found or not tagged to this admin." });
+      }
+  
+      if (action !== "accept" && action !== "reject") {
+        return res.status(400).json({ message: "Invalid action. Must be 'accept' or 'reject'." });
+      }
+  
+ 
+      const submission = assignment.submissions.find(
+        (sub) => sub.submittedTo.toString() === adminId.toString()
+      );
+  
+      if (!submission) {
+        return res.status(404).json({ message: "No submission found for this admin." });
+      }
+  
+      submission.status = action;
+      await assignment.save();
+  
+      res.status(200).json({ message: `Assignment ${action}ed successfully.`, assignment });
+    } catch (error) {
+      next(error);
+    }
+  };
--- server/src/modules/admin/routes/admin-route.js
+++ server/src/modules/admin/routes/admin-route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { register, login } from "../../../shared/auth/authController.js";
-
+import { getAssignmentsTagged,updateAssignmentStatus } from "../controllers/admin-controller.js";
 export const adminRoutes = express.Router();
 
 adminRoutes.post("/register", register);
 adminRoutes.post("/login", login);
+adminRoutes.get("/assignments", getAssignmentsTagged );
+adminRoutes.post("/assignments/:id/:action", updateAssignmentStatus);
 
