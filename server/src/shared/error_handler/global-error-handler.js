9d0cb740fc48cc8ae52c3abd8733677cb08a35be
Vanshika-Dargan <vanshikadargan.vd@gmail.com>
2024-10-10 17:54:43 +0530

    updated global error handler with stack trace

--- server/src/shared/error_handler/global-error-handler.js
+++ server/src/shared/error_handler/global-error-handler.js
@@ -1,10 +1,12 @@
 export default (error, req, res, next) => {
     const statusCode = error.statusCode || 500;
     
+    const stackLines = error.stack.split('\n');
     return res.status(statusCode).json({
       error:{
         code: statusCode,
-        message: error.message
+        message: error.message,
+        stackTrace: stackLines
       },
     });
   };
--- server/src/shared/utilities/token.js
+++ server/src/shared/utilities/token.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const SECRET = process.env.JWT_SECRET;
 const EXPIRY = process.env.JWT_EXPIRY;
